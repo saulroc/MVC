851d75509c6f8956cd69a14a1d4d808d8d1a9183
Carlos Pareja <saulroc@hotmail.com>
2022-05-02 12:02:29 +0200

    Añadimos restricciones a la clase película

--- Models/Pelicula.cs
+++ Models/Pelicula.cs
@@ -1,13 +1,19 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace MVC.Models
 {
     public class Pelicula
     {
         public int Id { get; set; }
+        
+        [Required]
+        [StringLength(500)]
         public string Titulo { get; set; }
         public DateTime FechaDeRealizacion { get; set; }
         public DateTime FechaDeRegistro { get; set; }
+        
+        [Range(1, 30)]
         public int Cantidad { get; set; }
         public byte GeneroId { get; set; }
         public virtual Genero Genero { get; set; }
